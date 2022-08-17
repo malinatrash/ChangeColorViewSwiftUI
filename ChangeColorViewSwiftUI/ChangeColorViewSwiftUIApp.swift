c1c0c9736d673760595990af2af9b74d5ae4d4e9
malinatrash <malinatrashh@gmail.com>
2022-08-17 18:36:20 +0800

    Initial Commit

---
+++ ChangeColorViewSwiftUI/ChangeColorViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeColorViewSwiftUIApp.swift
+//  ChangeColorViewSwiftUI
+//
+//  Created by Pavel Naumov on 17.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChangeColorViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
